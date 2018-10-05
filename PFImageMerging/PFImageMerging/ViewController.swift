d840f6d77e1b5ad56b60a8b779951fefe542417a
Sri Harsha Rudraraju <harshaios@outlook.com>
2018-10-05 13:07:02 -0500

    Initial Commit With Images
    Images without extension

---
+++ PFImageMerging/PFImageMerging/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PFImageMerging
+//
+//  Created by Sri Harsha Rudraraju on 10/5/18.
+//  Copyright © 2018 iOSPandaFactory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
